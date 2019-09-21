f226c43be65af5755cc152ffa56bbec940f1dd81
John McDonnell <john@renderlogic.com>
2019-09-21 13:31:56 -0400

    WDX-15: added lib for classes.
    Former-commit-id: 170f1c59153575e65425c9b73a8641afa01fcbfc

---
+++ lib/ArgRouter.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ArgRouter.cpp
+ * Author: pdarkstone
+ * 
+ * Created on September 21, 2019, 1:30 PM
+ */
+
+#include "ArgRouter.h"
+
+ArgRouter::ArgRouter() {
+}
+
+ArgRouter::ArgRouter(const ArgRouter& orig) {
+}
+
+ArgRouter::~ArgRouter() {
+}
+
---
+++ lib/ArgRouter.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ArgRouter.h
+ * Author: pdarkstone
+ *
+ * Created on September 21, 2019, 1:30 PM
+ */
+
+#ifndef ARGROUTER_H
+#define ARGROUTER_H
+
+class ArgRouter {
+public:
+    ArgRouter();
+    ArgRouter(const ArgRouter& orig);
+    virtual ~ArgRouter();
+private:
+
+};
+
+#endif /* ARGROUTER_H */
+
